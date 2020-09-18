e5cfac17312fa6a211bdda22f5a08d349c2a3e32
abhishek-bodapati <47183749+abhishek-bodapati@users.noreply.github.com>
2020-09-18 11:47:30 +0530

    Add files via upload

---
+++ payment.php
@@ -0,0 +1,32 @@
+<?php
+
+    $Name=$_POST['Name'];
+    $Email=$_POST['Email'];
+    $Amount=$_POST['Amount'];
+    $Phone=$_POST['phone'];
+    $purpose='Donation';
+    
+    include 'instamojo/Instamojo.php';
+    $api = new Instamojo\Instamojo('test_c37be77f39eda37250529ef5548', 'test_a59d83ba9c7bf774fb12f71e836', 'https://test.instamojo.com/api/1.1/');
+
+    try {
+        $response = $api->paymentRequestCreate(array(
+            "purpose" => $purpose,
+            "amount" => $Amount,
+            "send_email" => true,
+            "email" => $Email,
+            "buyer_name" =>$Name,
+            "phone"=>$Phone,
+            "send_sms" => true,
+            "allow_repeated_payments" =>false,
+            "redirect_url" => "http://localhost:8000/redirect.php"
+            )
+        );
+        $pay_url=$response['longurl'];
+        header("location: $pay_url");
+    	}
+    	catch (Exception $e) {
+    	    print('Error: ' . $e->getMessage());
+    	}
+?>
+
